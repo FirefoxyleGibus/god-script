eb613d00d04ecb6a8cd2253518049071be8ffba8
Louvtt <53311833+Louvtt@users.noreply.github.com>
2022-08-21 13:54:27 +0200

    Improved main to get a filename in the first cli argument.

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 # God script use .gsc
 
 from engine import *
-import time
 
-t1 = time.perf_counter()
-T = tokenizer("test.gsc")
-P = parser()
-I = interpreter()
-t2 = time.perf_counter()
-A = I.exec(P.parse(T.sendData()))
-t3 = time.perf_counter()
+# Entry point
+def main(args):
+    import time
 
-print("\n\n=========")
-print("Data read in {0:.5f}s\nExecuted in {1:.5f}s\nTotal time spent {2:.5f}s".format(t2-t1,t3-t2,t3-t1))
-input("Press enter to close the process ... ")
+    # Find filename else raise error
+    filename = "" # default to test.gsc
+    for arg in args:
+        if arg.split(".")[1] == "gsc":
+            filename = arg
+    if not filename:
+        raise SyntaxError("Please specify a filename")
+
+    t1 = time.perf_counter()
+    T = tokenizer(filename)
+    P = parser()
+    I = interpreter()
+    t2 = time.perf_counter()
+    I.exec(P.parse(T.sendData()))
+    t3 = time.perf_counter()
+
+    print("\n\n=========")
+    print("Data read in {0:.5f}s\nExecuted in {1:.5f}s\nTotal time spent {2:.5f}s".format(t2-t1,t3-t2,t3-t1))
+    input("Press enter to close the process ... ")
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
