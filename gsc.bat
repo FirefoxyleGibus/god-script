ca4a25daf19a94f868b42a1d97326a168341a459
Louvtt <53311833+Louvtt@users.noreply.github.com>
2022-08-26 15:05:25 +0200

    Added --debug flag

--- gsc.bat
+++ gsc.bat
@@ -4,9 +4,10 @@ goto :init
 :help
     echo (You must run this in the current directory where the main.py is)
     echo Usage: "runGodScript.bat [options]"
-    echo    -f      Target file to run
-    echo    -h      Show this prompt
-    echo    -git    Show the link to the github page
+    echo    -f        Target file to run
+    echo    -h        Show this prompt
+    echo    -git      Show the link to the github page
+    echo    --debug   Show the debug log
     echo.
     goto :end
 
@@ -23,7 +24,8 @@ goto :init
     echo Trying to run "%~1"
     if not exist %~1 call :missing_file & goto :end
 
-    py main.py %~1
+    if /i "%~2" == "--debug" py main.py %~1 & py log_debug.py & goto :end
+    py main.py %~1 & goto:end
     goto :end
 
 :missing_argument
@@ -39,10 +41,13 @@ goto :init
     if /i "%~1"=="-git"  call :show_git & goto :end
     if /i "%~1"=="/git"  call :show_git & goto :end
 
+    @REM Show debug stacktrace
+    if /i "%~1" == "--debug" py log_debug.py & goto :end
+
     @REM Running a gsc program
-    if "%~2"==""       call :missing_argument & goto :end
-    if /i "%~1"=="/f"    call :run_script %~2 & goto :end
-    if /i "%~1"=="-f"    call :run_script %~2 & goto :end
+    if "%~2"==""         call :missing_argument & goto :end
+    if /i "%~1"=="/f"    call :run_script %~2 %~3 & goto :end
+    if /i "%~1"=="-f"    call :run_script %~2 %~3 & goto :end
     GOTO end
 
 :init
---
+++ log_debug.py
@@ -0,0 +1,5 @@
+# Debug output
+
+if __name__ == "__main__":
+    with open("__gsccache__/stacktrace.txt", "r") as f:
+        print("\nDebugger stacktrace:\n",f.read())
