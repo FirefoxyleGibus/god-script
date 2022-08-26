f900e3f59d10be2e48b64b3b3be9b1a264b2671d
Louvtt <53311833+Louvtt@users.noreply.github.com>
2022-08-26 15:05:57 +0200

    Passing debug flag to debugger

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,30 +1,33 @@
 from engine.engine import *
 
-def setup():
-    from engine.debugger import Debugger
-    Debugger.init()
+def setup(debug_mode):
+	from engine.debugger import Debugger
+	Debugger.init(debug_mode)
 
 # Entry point
 def main(args):
-    import time
-    # Find filename else raise error
-    filename = "" # default to test.gsc
-    for arg in args:
-        if arg.split(".")[1] == "gsc":
-            filename = arg
-    if not filename:
-        raise SyntaxError("Please specify a filename")
+	import time
+	# Find filename else raise error
+	filename = "" # default to test.gsc
+	debug_mode = False
+	for arg in args:
+		if ".gsc" in arg: # if gsc file
+			filename = arg
+		if arg == "--debug": # if debug active
+			debug_mode = True
+	if not filename:
+		raise SyntaxError("Please specify a filename")
 
-    setup()
+	setup(debug_mode)
 
-    t1 = time.perf_counter()
-    T = tokenizer(filename)
-    P = parser()
-    I = interpreter()
-    t2 = time.perf_counter()
-    I.exec(T.sendData())
-    t3 = time.perf_counter()
+	t1 = time.perf_counter()
+	T = tokenizer(filename)
+	P = parser()
+	I = interpreter()
+	t2 = time.perf_counter()
+	I.exec(T.sendData())
+	t3 = time.perf_counter()
 
-    print("\n\n=========")
-    print("Data read in {0:.5f}s\nExecuted in {1:.5f}s\nTotal time spent {2:.5f}s".format(t2-t1,t3-t2,t3-t1))
-    input("Press enter to close the process ... ")
+	print("\n\n=========")
+	print("Data read in {0:.5f}s\nExecuted in {1:.5f}s\nTotal time spent {2:.5f}s".format(t2-t1,t3-t2,t3-t1))
+	input("Press enter to close the process ... ")
--- engine/debugger.py
+++ engine/debugger.py
@@ -1,81 +1,90 @@
 import os
 
 class Debugger:
-    def init(indent="\t"):
-        """Initialize the debugger, must be called before any of its methods.
+	def init(debug_mode, indent="\t"):
+		"""Initialize the debugger, must be called before any of its methods.
 
-        Args:
-            indent (str, optional): indentation character for sections. Defaults to "\t".
-        """
-        if not (os.path.isdir(r"./__gsccache__")):
-            os.mkdir(r"./__gsccache__")
-        Debugger.file = open("__gsccache__/stacktrace.txt", "w")
-        Debugger.current_sections_stack = []
-        Debugger.indent_str = indent
+		Args:
+			indent (str, optional): indentation character for sections. Defaults to "\t".
+		"""
+		Debugger.debug_mode = debug_mode
+		if not (os.path.isdir(r"./__gsccache__")):
+			os.mkdir(r"./__gsccache__")
+		Debugger.file = open("__gsccache__/stacktrace.txt", "w")
+		Debugger.current_sections_stack = []
+		Debugger.indent_str = indent
+	
+	def _write_output(output):
+		"""Write to file and to stdout if out is true"""
+		s = Debugger._indent() + output + "\n"
+		if not Debugger.file.closed:
+			Debugger.file.write(s)
 
-    def _indent():
-        """Return the Indentation based on how many sections are opened
+	def _indent():
+		"""Return the Indentation based on how many sections are opened
 
-        Returns str
-        """
-        return len(Debugger.current_sections_stack) * Debugger.indent_str
+		Returns str
+		"""
+		return len(Debugger.current_sections_stack) * Debugger.indent_str
 
-    def begin_section(section_name):
-        """Begin a new section
+	def begin_section(section_name):
+		"""Begin a new section
 
-        Args:
-            section_name (str): Section name
-        """
-        Debugger.file.write(Debugger._indent() + f"[ BEGIN SECTION {section_name} ]\n")
-        Debugger.current_sections_stack.append(section_name)
+		Args:
+			section_name (str): Section name
+		"""
+		s = f"[ BEGIN SECTION {section_name} ]"
+		Debugger._write_output(s)
+		Debugger.current_sections_stack.append(section_name)
 
-    def end_section():
-        """End the last opened section
+	def end_section():
+		"""End the last opened section
 
-        Returns None
-        """
-        if len(Debugger.current_sections_stack) == 0: return # No section to close
-        section_name = Debugger.current_sections_stack.pop()
-        Debugger.file.write(Debugger._indent() + f"[ ENDING SECTION {section_name} ]\n")
+		Returns None
+		"""
+		if len(Debugger.current_sections_stack) == 0: return # No section to close
+		section_name = Debugger.current_sections_stack.pop()
 
-    def log(*msg, sep=" "):
-        """Log the message to the debug file
+		s = f"[ ENDING SECTION {section_name} ]"
+		Debugger._write_output(s)
 
-        Args:
-            sep (str, optional): separation between msg elements. Defaults to " ".
-        """
-        s = str(sep).join(msg)
-        Debugger.file.write(Debugger._indent() + s + "\n")
+	def log(*msg, sep=" "):
+		"""Log the message to the debug file
 
-    def log_error(*msg, sep=" "):
-        """Log error to the debug file
+		Args:
+			sep (str, optional): separation between msg elements. Defaults to " ".
+		"""
+		s = str(sep).join(msg)
+		Debugger._write_output(s)
 
-        Args:
-            sep (str, optional): separation between msg elements. Defaults to " ".
-        """
-        s = str(sep).join(msg)
-        Debugger.file.write(Debugger._indent() + "[ERROR] " + s + "\n")
+	def log_error(*msg, sep=" "):
+		"""Log error to the debug file
 
-    def log_instruction(instruction, *msg, sep=" "):
-        s = instruction.instr + " "
-        s += f"[{str(instruction.filepos)}]: " + sep.join([str(p) for p in msg])
-        Debugger.file.write(Debugger._indent() + s + "\n")
+		Args:
+			sep (str, optional): separation between msg elements. Defaults to " ".
+		"""
+		s = "[ERROR] " + str(sep).join(msg)
+		Debugger._write_output(s)
 
-    def log_instruction_error(instruction, *msg, sep=" "):
-        s = "[ERROR] " + instruction.instr + " "
-        s += f"[{str(instruction.filepos)}]: " + sep.join([str(p) for p in msg])
-        Debugger.file.write(Debugger._indent() + s + "\n")
+	def log_instruction(instruction, *msg, sep=" "):
+		s  = instruction.instr + " "
+		s += f"[{str(instruction.filepos)}]: " + sep.join([str(p) for p in msg])
+		Debugger._write_output(s)
 
+	def log_instruction_error(instruction, *msg, sep=" "):
+		s = "[ERROR] " + instruction.instr + " "
+		s += f"[{str(instruction.filepos)}]: " + sep.join([str(p) for p in msg])
+		Debugger._write_output(s)
 
-    def log_func_call(funcname, parameters_list):
-        """Log the call to the <funcname> with their parameters
+	def log_func_call(funcname, parameters_list):
+		"""Log the call to the <funcname> with their parameters
 
-        Args:
-            funcname (str): function name that has been called
-            parameters_list (list): list of the parameters that was given for this function call
-        """
-        Debugger.file.write(Debugger._indent() + f"Called {funcname} with {', '.join([repr(p) for p in parameters_list])}\n")
+		Args:
+			funcname (str): function name that has been called
+			parameters_list (list): list of the parameters that was given for this function call
+		"""
+		Debugger._write_output(f"Called {funcname} with {', '.join([repr(p) for p in parameters_list])}")
 
-    def close():
-        """Close the Debugger (must be called at the end of the program)"""
-        Debugger.file.close()
+	def close():
+		"""Close the Debugger (must be called at the end of the program)"""
+		Debugger.file.close()
