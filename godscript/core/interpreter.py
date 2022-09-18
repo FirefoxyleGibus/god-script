2c3be951ba5d813807f446b7b3b3c13f17ae6650
Foxy <maxence.plisson@outlook.com>
2022-09-18 14:41:39 +0200

    First draft of the interpreter
    it's bad

---
+++ godscript/core/interpreter.py
@@ -0,0 +1,48 @@
+class Interpreter:
+    def cutInstr(self,inp):
+        self.instr = []
+        BUFF = ""
+        strFlag = False
+        counterslashFlag = False
+        for i in inp:
+            if (strFlag):
+                if (i == '"'):
+                    strFlag = False
+                BUFF += i
+            else:
+                if (i == '"'):
+                    strFlag = True
+                elif (i == " "):
+                    self.instr.append(BUFF)
+                    BUFF = ""
+                    continue
+                BUFF += i
+        self.instr.append(BUFF)
+        return self.instr
+
+    def readInstr(self):
+        funcFlag = False
+        paramsFlag = False
+        func = ""
+        params = []
+        for i in self.instr:
+            if not funcFlag:
+                if (i == "f"):
+                    funcFlag = True
+            else:
+                if (i == "f"):
+                    print(f"interpreted {func} with ["+",".join(params)+"]")
+                    paramsFlag = False
+                    func = ""
+                    params = []
+                elif not paramsFlag:
+                    func = i
+                    paramsFlag = True
+                else:
+                    params.append(i)
+        else:
+            print(f"interpreted {func} with ["+",".join(params)+"]")
+            paramsFlag = False
+            func = ""
+            
+
